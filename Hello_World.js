3a51df57a5fa518bebf314a2b3f97c8e45848e2b
Shivakumar Mahakali <mahakalim@yahoo.com>
2019-09-12 20:34:12 -0400

    Added extra comments on lines 11-12

--- Hello_World.js
+++ Hello_World.js
@@ -7,3 +7,5 @@ Creates multi-line comments in JavaScript
 // same as C-lang
 
 console.log("Hello World!");
+
+// Though JS does not require the ; at the end of statements, it is good programming practice to include it.
